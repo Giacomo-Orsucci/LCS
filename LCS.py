7157196509b7ac923c7f0f975fe8426c227212f6
orsuc <orsucci.giacomo@gmail.com>
2023-03-04 20:01:54 +0100

    brute_force method just added

--- LCS.py
+++ LCS.py
@@ -3,23 +3,35 @@ class LCS:
     def __init__(self, sequence1):
         self.sequence1 = sequence1
         self.result = ""
-
+        self.length = 0
 
     def brute_force(self, sequence2): #wrong, to fix
-        for x in self.sequence1:
-            for y in sequence2:
-                if x == y:
-                    self.result += x
-            sequence2 = sequence2[1:]
-        print(self.result)
+        LCS_length = 0
+        for i in range(len(self.sequence1)):
+            j = i
+            temp = ""
+            temp_length = 0
+            for j in range(len(self.sequence1)):
+                found = False
+                for k in range(len(sequence2)):
+                    if self.sequence1[j] == sequence2[k] and found is False:
+                        temp += sequence2[k]
+                        temp_length += 1
+                        found = True
+            if temp_length > len(self.result):
+                self.result = temp
+                self.length = temp_length
 
+        return LCS_length
     def recursive(self, sequence2, i, j):
         if i < 0 or j < 0:
             return 0
         if self.sequence1[i] == sequence2[j]:
             return 1+self.recursive(sequence2, i-1, j-1)
+
         else:
             return max(self.recursive(sequence2, i, j-1), self.recursive(sequence2, i-1, j))
+
 
 
 
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from LCS import *
 
 
-lcs1 = LCS("cantiere")
-sequence2 = "armiere"
+lcs1 = LCS("quaderno")
+sequence2 = "quadro"
+#
+#length = lcs1.recursive(sequence2, len(lcs1.sequence1)-1, len(sequence2)-1)
+#print(length)
+#print(lcs1.result)
 
-length = lcs1.recursive(sequence2, len(lcs1.sequence1)-1, len(sequence2)-1)
-print(length)
+length = lcs1.brute_force(sequence2)
+print(lcs1.length)
+print(lcs1.result)
